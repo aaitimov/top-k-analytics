76b0ce19557b1c1a637cf7d3ca686075637705e2
Anuar Aitimov <anuar.aitimov@gmail.com>
2025-07-31 01:36:41 +0500

    chore: restructure event generator sub-project

--- event-generator/app/main.py
+++ event-generator/app/main.py
@@ -8,7 +8,7 @@ from concurrent.futures import ThreadPoolExecutor
 
 from confluent_kafka import Producer
 
-from generator import SongPlayEventGenerator
+from app.generator import SongPlayEventGenerator
 
 logging.basicConfig(level=logging.INFO, format="[%(threadName)s] %(message)s")
 
